02496ba6de429e949d63638f4ceafb84c642b170
vishwas <vishwas452003@gmail.com>
2025-06-07 20:29:54 +0530

    three

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import * as THREE from "three";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.150.1/build/three.module.js";
+
+// rest of your code...
 
 // Global vars
 let scene, camera, renderer;
